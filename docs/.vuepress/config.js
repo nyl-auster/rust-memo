16bd4eca18a1b5354673353214fc17a0e56c6ee1
yann <yann.boisselier@gmail.com>
2018-09-15 17:17:43 +0200

    use vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: 'Rust depuis zéro',
+  description: 'Apprendre Rust progressivement depuis zéro',
+  themeConfig: {
+    sidebar: [
+      '/',
+      '/01-prelude',
+      '/02-prelude-compilation-execution',
+      '/03-prelude-fichier-binaire',
+      '/04-prelude-bit',
+      '/05-prelude-numeration-binaire',
+      '/06-prelude-gestion-memoire',
+      '/07-prelude-les-segments-memoire',
+      '/08-commencer-rust-rapidement',
+      '/09-cargo',
+      '/10-variables',
+      '/11-duree-de-vie-des-variables',
+      '/12-boucles',
+      '/13-conditions',
+      '/14-propriete-et-emprunt'
+    ]
+  }
+}
