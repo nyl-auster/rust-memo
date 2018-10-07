ce52683a91f5ed9977640fe8875e57a8b07ab02f
yann <yann.boisselier@gmail.com>
2018-10-07 19:46:04 +0200

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,10 @@ npm run docs:build
 # navigate into the build output directory
 cd docs/.vuepress/dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
 git push -f git@github.com:larouille/larouille.github.io master
 
 cd -
